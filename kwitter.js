cd3473beb1c40bb2c0e397920428cd69ca7075d5
nandithagitub <78301156+nandithagitub@users.noreply.github.com>
2021-09-10 16:45:13 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,7 @@
+
+function adduser() {
+    var user_name = document.getElementById("username").value;
+    localStorage.setItem("username", user_name);
+    window.location = "kwitter_room.html";
+}
+
